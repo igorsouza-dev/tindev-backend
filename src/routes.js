e5b29557ebc891dc086925d80377966f09c31245
Igor Souza <igorsouza.dev@gmail.com>
2019-08-10 18:04:47 -0300

    Adding the Devs model

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
@@ -14,8 +15,6 @@ routes.get('/', (req, res) => {
     return res.json({message: message});
 });
 
-routes.post('/devs', (req, res) => {
-    return res.json(req.body);
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
