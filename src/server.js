065f7204ddd3bea7e4f46dbcc974f5109b69f0ad
Igor Souza <igorsouza.dev@gmail.com>
2019-08-10 16:40:47 -0300

    Testing Express server

---
+++ src/server.js
@@ -0,0 +1,17 @@
+const express =  require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    const query = req.query;
+    let message = 'Hello, ';
+    if(query.name) {
+        message += query.name;
+    } else {
+        message += 'World';
+    }
+    message += '!';
+    return res.send(message);
+});
+
+server.listen(8080);
