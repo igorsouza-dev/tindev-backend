024d65a79a86103ee60b17353fa583ea38fd6f33
Igor Souza <igorsouza.dev@gmail.com>
2019-08-10 17:53:55 -0300

    Adding mongodb connection

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # tindev-backend
 
 A NodeJS backend made for the Tindev app
+
+In order for the server to connect to the Mongodb database, you should add a `config.js` file at the root of the project. This file should export a `db_uri` variable.
--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 const express =  require('express');
 const routes = require('./routes');
+const mongoose = require('mongoose');
+const config = require('../config');
 
 const server = express();
+
+mongoose.connect(config.db_uri, { useNewUrlParser: true });
+
 server.use(express.json());
 server.use(routes);
 
