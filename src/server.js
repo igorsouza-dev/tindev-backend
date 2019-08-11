f079ae47d4816091b8b5020080342cedbc97d5fe
Igor Souza <igorsouza.dev@gmail.com>
2019-08-11 10:43:19 -0300

    Added cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,12 @@ const express =  require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const config = require('../config');
-
+const cors = require('cors');
 const server = express();
 
 mongoose.connect(config.db_uri, { useNewUrlParser: true });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
